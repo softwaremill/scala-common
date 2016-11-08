87d375f64691024967a272e1a26b7390b1b3c00f
Marcin Kubala <marcin@mkubala.pl>
2016-11-08 12:41:33 +0100

    Add covariance annotation for tagged types
    This commit puts the tagged type `T` in a covariant position which makes tagging irrelevant while reasoning about inheritance.
    
    Given we have two types:
    trait A
    trait B extends A
    
    and some tag:
    trait Tag
    
    then:
    B @@ Tag should be considered as a subtype of A @@ Tag

--- tagging/src/main/scala/com/softwaremill/tagging/package.scala
+++ tagging/src/main/scala/com/softwaremill/tagging/package.scala
@@ -23,8 +23,8 @@ package com.softwaremill
   */
 package object tagging {
   type Tag[+U] = { type Tag <: U }
-  type @@[T, +U] = T with Tag[U]
-  type Tagged[T, +U] = T with Tag[U]
+  type @@[+T, +U] = T with Tag[U]
+  type Tagged[+T, +U] = T with Tag[U]
   implicit class Tagger[T](t: T) {
     def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
   }
