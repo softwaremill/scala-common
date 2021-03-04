2fe073b25732d6da91610a53215aaebea63c4b7a
Boris Smidt <boris.smidt@restore.eu>
2021-03-04 20:05:37 +0100

    Add type tagging support for F types to make tagging Seq, Future etc easier

--- tagging/src/main/scala/com/softwaremill/tagging/package.scala
+++ tagging/src/main/scala/com/softwaremill/tagging/package.scala
@@ -1,5 +1,7 @@
 package com.softwaremill
 
+import scala.language.higherKinds
+
 /**
   * Tag instances with arbitrary types. The tags are usually empty `trait`s. Tags have no runtime overhead and are only
   * used at compile-time for additional type safety.
@@ -23,12 +25,38 @@ package com.softwaremill
   */
 package object tagging {
   trait Tag[+U] extends Any { type Tag <: U }
-  type @@[+T, +U]     = T with Tag[U]
+  type @@[+T, +U] = T with Tag[U]
   type Tagged[+T, +U] = T with Tag[U]
+
   implicit class Tagger[T](val t: T) extends AnyVal {
-    def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
+    @inline def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
   }
   implicit class AndTagger[T, U](val t: T @@ U) extends AnyVal {
-    def andTaggedWith[V]: T @@ (U with V) = t.asInstanceOf[T @@ (U with V)]
+    @inline def andTaggedWith[V]: T @@ (U with V) = t.asInstanceOf[T @@ (U with V)]
+
+    @inline def replaceTag[V]: T @@ V = t.asInstanceOf[T @@ V]
+    @inline def eraseTag: T = t.asInstanceOf[T]
+  }
+
+  implicit class TaggingF[F[_], T](val fa: F[T]) extends AnyVal {
+    @inline def taggedWithF[B]: F[T @@ B] = fa.asInstanceOf[F[T @@ B]]
+  }
+
+  implicit class AndTaggingF[F[_], T, U](val ft: F[T @@ U]) extends AnyVal {
+    @inline def eraseTagF: F[T] = ft.asInstanceOf[F[T]]
+
+    @inline def andTaggedWithF[V]: F[T @@ (U with V)] = ft.asInstanceOf[F[T @@ (U with V)]]
+    @inline def replaceTag[V]: F[T @@ V] = ft.asInstanceOf[F[T @@ V]]
+  }
+
+  implicit class TaggingF2[F1[_], F2[_], T](val ft: F1[F2[T]]) extends AnyVal {
+    @inline def taggedWithF2[B]: F1[F2[T @@ B]] = ft.asInstanceOf[F1[F2[T @@ B]]]
+  }
+
+  implicit class AndTaggingF2[F1[_], F2[_], T, U](val ft: F1[F2[T @@ U]]) extends AnyVal {
+    @inline def eraseTagF2: F1[F2[T]] = ft.asInstanceOf[F1[F2[T]]]
+
+    @inline def replaceTagF2[V]: F1[F2[T @@ V]] = ft.asInstanceOf[F1[F2[T @@ V]]]
+    @inline def andTaggedWithF2[V]: F1[F2[T @@ (U with V)]] = ft.asInstanceOf[F1[F2[T @@ (U with V)]]]
   }
 }
