2f4750b37b75c461df41166da598610cd46be9ef
Scala Steward <me@scala-steward.org>
2021-08-27 22:39:20 +0200

    Reformat with scalafmt 3.0.1

--- futureSquash/src/main/scala/com/softwaremill/futuresquash/FutureSquash.scala
+++ futureSquash/src/main/scala/com/softwaremill/futuresquash/FutureSquash.scala
@@ -5,12 +5,11 @@ import scala.util._
 
 object FutureSquash {
 
-  /**
-   * Converts an Either[Throwable, A] to a Future[A] that may raise a Throwable
-   */
+  /** Converts an Either[Throwable, A] to a Future[A] that may raise a Throwable
+    */
   def fromEither[A](either: Either[Throwable, A]): Future[A] = {
     either match {
-      case Right(a) => Future.successful(a)
+      case Right(a)    => Future.successful(a)
       case Left(error) => Future.failed(error)
     }
   }
@@ -23,13 +22,11 @@ object FutureSquash {
     def squash(implicit ec: ExecutionContext): Future[A] = futureTryStack.flatMap(Future.fromTry)
   }
 
-  /**
-   * Converts an Option[A] to a Future[A] that may raise a NoSuchElementException
-   *
-   */
+  /** Converts an Option[A] to a Future[A] that may raise a NoSuchElementException
+    */
   def fromOption[A](option: Option[A]): Future[A] = option match {
     case Some(a) => Future.successful(a)
-    case None => Future.failed(new NoSuchElementException)
+    case None    => Future.failed(new NoSuchElementException)
   }
 
   implicit class FutureOption[A](futureOptionStack: Future[Option[A]]) {
