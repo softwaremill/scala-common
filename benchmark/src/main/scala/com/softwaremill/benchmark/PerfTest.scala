808d14d6aa3392cc5ca143fbcc27c206d4b857d7
Scala Steward <me@scala-steward.org>
2021-09-25 20:28:05 +0200

    Reformat with scalafmt 3.0.5

--- benchmark/src/main/scala/com/softwaremill/benchmark/PerfTest.scala
+++ benchmark/src/main/scala/com/softwaremill/benchmark/PerfTest.scala
@@ -2,23 +2,19 @@ package com.softwaremill.benchmark
 
 import scala.util.Try
 
-/**
-  * Represents a single performance test.
+/** Represents a single performance test.
   */
 trait PerfTest {
 
-  /**
-    * A warmup that should be executed before each single test execution.
+  /** A warmup that should be executed before each single test execution.
     */
   def warmup(): Unit = ()
 
-  /**
-    * Synchronous test body.
+  /** Synchronous test body.
     */
   def run(): Try[String]
 
-  /**
-    * Test name (will be used in reports.
+  /** Test name (will be used in reports.
     */
   def name: String
 }
--- futureTry/src/main/scala/com/softwaremill/futuretry/package.scala
+++ futureTry/src/main/scala/com/softwaremill/futuretry/package.scala
@@ -7,16 +7,16 @@ package object futuretry {
 
   implicit class FutureTry[T](val baseFuture: Future[T]) extends AnyVal {
 
-    /**
-      * Reifies the Try output of Futures. Useful mostly for interaction with unusual APIs.
-      * @return the same future with its result wrapped up in `Try`.
+    /** Reifies the Try output of Futures. Useful mostly for interaction with unusual APIs.
+      * @return
+      *   the same future with its result wrapped up in `Try`.
       */
     def tried(implicit executor: ExecutionContext): Future[Try[T]] = {
       baseFuture.map(Success.apply).recover { case f => Failure(f) }.mapTo[Try[T]]
     }
 
-    /**
-      * @return a new future with the original's result applied by `f`.
+    /** @return
+      *   a new future with the original's result applied by `f`.
       */
     def transformTry[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext): Future[S] = {
       val p = Promise[S]
