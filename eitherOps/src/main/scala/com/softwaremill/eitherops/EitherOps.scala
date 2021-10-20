7da259daf80988abb08030b079f9f70bb96543e9
Scala Steward <me@scala-steward.org>
2021-10-20 20:21:13 +0200

    Reformat with scalafmt 3.0.7

--- eitherOps/src/main/scala/com/softwaremill/eitherops/EitherOps.scala
+++ eitherOps/src/main/scala/com/softwaremill/eitherops/EitherOps.scala
@@ -2,31 +2,30 @@ package com.softwaremill.eitherops
 
 object EitherOps {
 
-  /**
-    * Retrieves Left values for several Either values
-    *  {{{
-    *  case class Person(firstName: String, lastName: String, age: Int)
+  /** Retrieves Left values for several Either values
+    * {{{
+    *   case class Person(firstName: String, lastName: String, age: Int)
     *
-    *  def validateAge(intValue: Int): Either[String, Int] = ???
-    *  def validateFirstName(stringValue: String): Either[String, String] = ???
-    *  def validateLastName(stringValue: String): Either[String, String] = ???
+    *   def validateAge(intValue: Int): Either[String, Int] = ???
+    *   def validateFirstName(stringValue: String): Either[String, String] = ???
+    *   def validateLastName(stringValue: String): Either[String, String] = ???
     *
-    *  val errors: Seq[String] = EitherOps.collectLefts(
+    *   val errors: Seq[String] = EitherOps.collectLefts(
     *   validateFirstName("john"),
     *   validateLastName("doe"),
     *   validateAge(40)
-    *  )
-    *  if (errors.isEmpty) ???// use for comprehension here to build a Person
-    *  else ??? // handle errors here
-    *  }}}
+    *   )
+    *   if (errors.isEmpty) ???// use for comprehension here to build a Person
+    *   else ??? // handle errors here
+    * }}}
     */
-  def collectLefts[A](eithers: Either[A, _]*): Seq[A] = eithers.collect {
-    case Left(a) => a
+  def collectLefts[A](eithers: Either[A, _]*): Seq[A] = eithers.collect { case Left(a) =>
+    a
   }
 
   /** Retrieves Right values for several Either values
     */
-  def collectRights[B](eithers : Either[_, B]*) : Seq[B]=  eithers.collect {
-    case Right(b) => b
+  def collectRights[B](eithers: Either[_, B]*): Seq[B] = eithers.collect { case Right(b) =>
+    b
   }
 }
